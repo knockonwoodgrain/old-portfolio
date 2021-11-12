5382819e3dd4f7059b285e1b5bb5b3bb85c84591
Parth Joshi <manotijoshi15@gmail.com>
2021-11-13 01:32:39 +0530

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,9 @@
 const btn = document.querySelector(".btn-toggle");
 const btnn = document.querySelector(".btn-toggle-phone");
 const theme = document.querySelector("#theme-link");
-const device = document.querySelector("#device-link")
+const device = document.querySelector("#device")
 var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-if (width<900){
+if (width<=900){
   device.href = "style/phone.css"
 }
 btn.addEventListener("click", function () {
@@ -20,50 +20,15 @@ btnn.addEventListener("click", function () {
   if (theme.getAttribute("href") == "style/variables-dark.css") {
     theme.href = "style/variables-light.css";
   }
+})
+$( "#myBtn" ).click(function() {
+  $( ".more" ).slideToggle("slow");
+  var $this = $(this);
+        $this.toggleClass("open");
+
+        if ($this.hasClass("open")) {
+            $this.html("Read Less");
+        } else {
+            $this.html("Read more");
+        }
 });
-
-function myFunction() {
-  var dots = document.getElementById("dots");
-  var moreText = document.getElementById("more");
-  var btnText = document.getElementById("myBtn");
-  var before = document.getElementById("before");
-  var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-  var overlay = document.getElementById("overlay")
-  console.log(width);
-  if (width >= 700) {
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more"; 
-      moreText.style.display = "none";
-      before.style.fontSize = "1.05vw";
-      dots.style.fontSize = "1.05vw";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less"; 
-      moreText.style.display = "inline";
-      moreText.style.fontSize = "1.2vw";
-      dots.style.fontSize = "1.2vw";
-      before.style.fontSize = "1.2vw"
-    }
-  }
-  else {
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more"; 
-      moreText.style.display = "none";
-      before.style.fontSize = "3vw";
-      dots.style.fontSize = "3vw";
-      overlay.style.minHeight = "100%"
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less"; 
-      moreText.style.display = "inline";
-      moreText.style.fontSize = "3.2vw";
-      dots.style.fontSize = "3.2vw";
-      before.style.fontSize = "3.2vw"
-      overlay.style.minHeight = "450%"
-    }
-  }
-}
-
-
