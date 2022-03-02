1d9f1829c107869241321de83347178aadc754d6
parthjoshioriginal <parthnjoshi33@gmail.com>
2022-03-02 15:27:37 +0530

    Revert "Create index.html"
    This reverts commit 562359619b12dc6dcc0384390a044361dfdec435.

--- scripts/script.js
+++ scripts/script.js
@@ -45,3 +45,11 @@ $( "#FOSSbutton" ).click(function() {
             $this.html("Read more");
         }
 });
+document.getElementById("body").onscroll = function myFunction() {  
+    var scrolltotop = document.scrollingElement.scrollTop;
+    var target = document.getElementById("main1");
+    var xvalue = "center";
+    var factor = 0.5;
+    var yvalue = scrolltotop * factor;
+    target.style.backgroundPosition = xvalue + " " + yvalue + "px";
+  }
